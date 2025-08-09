84cdff47d2f6d6b5b51ae45427ca451ae0843358
andrecruz0 <andrebrbug@gmail.com>
2025-08-09 11:55:43 -0300

    Add CORS support and update package dependencies

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,13 @@ import express from "express";
 import { productsRouter } from "./routes/products.routes";
 import { connect } from "./db/connectdb";
 import { connectionMiddleware } from "./middleware/connectionMiddleware";
+import cors from 'cors';
 const app = express()
 app.use(express.json());
 app.use(connectionMiddleware)
+app.use(cors({
+  origin: 'http://localhost:3001' 
+}));
 
 app.use(productsRouter)
 
